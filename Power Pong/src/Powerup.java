07cf1547965af9164568abd2e25f252ff7e080ed
rbagdasarian <rachelbags@comcast.net>
2017-06-06 10:32:39 -0400

    maybe?

--- Power Pong/src/Powerup.java
+++ Power Pong/src/Powerup.java
@@ -1,8 +1,5 @@
-/**
- * Created by wande18094 on 5/26/2017.
- */
 public class Powerup {
-    private Racket rack1, rack2;
+    private static Racket rack1, rack2;
 
     public Powerup(String powerup, Racket rack1, Racket rack2){
         this.rack1 = rack1;
@@ -15,11 +12,11 @@ public class Powerup {
         if (powerup == "iSize2") iSize2();
     }
 
-    public void iSize1(){
-        rack1.setHeight(PongPanel.player1.getHeight() + 20);
+    public static void iSize1(){
+        rack2.setHeight(PongPanel.player2.getHeight() + 20);
     }
 
-    public void iSize2(){
-        rack2.setHeight(PongPanel.player2.getHeight() + 20);
+    public static void iSize2(){
+        rack1.setHeight(PongPanel.player1.getHeight() + 20);
     }
 }
